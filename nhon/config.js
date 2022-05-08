8539486d841ef6f658b415edd6101412a1712b4c
DucLong06 <hoangduclongg@gmail.com>
2022-05-08 11:59:09 +0700

    update

---
+++ nhon/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Hello Thuỳ Ly",
+    introTitle: 'Quang Thị Thuỳ Ly',
+    introDesc: `Con nhợn kia`,
+    btnIntro: 'Click vào đây nè',
+    title: 'Phải chăng Ly đã THÍCH ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Ly đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại THÍCH mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'THÍCH thì THÍCH mà không THÍCH thì THÍCH <33333333',
+    mess: 'Mình biết mà 🥰. THÍCH bạn nhiều nhiều 😘😘',
+    messDesc: 'chiều nay 3h, mình đi ăn phở cuốn nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/duclong2k' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
